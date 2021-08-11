eb48423edf55e3ce6c9c40ccde9b5afd45beeb1d
LucyMoon <rjf5907@daum.net>
2021-08-11 20:32:46 +0900

    bottom_navigation
    바텀네비게이션 완성
    + 프래그먼트 틀 완성

--- EatItDogAndroid/app/src/main/java/com/example/eatitdogandroid/MainActivity.kt
+++ EatItDogAndroid/app/src/main/java/com/example/eatitdogandroid/MainActivity.kt
@@ -2,12 +2,79 @@ package com.example.eatitdogandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toolbar
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
+    //
+    private lateinit var scanFragment:ScanFragment
+    private lateinit var searchFragment:SearchFragment
+    private lateinit var hospitalFragment: HospitalFragment
+
+    companion object{
+
+        const val TAG : String = "로그"
 
     }
+
+    //메모리에 올라갔을 때
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        //레이아웃과 연결
+        setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "MainActivity - onCreate() called")
+
+        bottomNavi.setOnNavigationItemSelectedListener(onBottomNavItemSelectedListener)
+
+        scanFragment = ScanFragment.newInstance()
+        supportFragmentManager.beginTransaction().add(R.id.main_frame, scanFragment).commit()
+    }
+    //바텀네비게이션 아이템 클릭 리스너 설정
+    private val onBottomNavItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener {
+        when(it.itemId){
+            R.id.item_scan ->{
+                Log.d(TAG, "MainActivity - 스캔버튼!")
+                scanFragment = ScanFragment.newInstance()
+                supportFragmentManager.beginTransaction().replace(R.id.main_frame, scanFragment).commit()
+            }
+            R.id.item_serch ->{
+                Log.d(TAG, "MainActivity - 검색버튼!")
+                searchFragment = SearchFragment.newInstance()
+                supportFragmentManager.beginTransaction().replace(R.id.main_frame, searchFragment).commit()
+            }
+            R.id.item_hospital ->{
+                Log.d(TAG, "MainActivity - 병원버튼!")
+                hospitalFragment = HospitalFragment.newInstance()
+                supportFragmentManager.beginTransaction().replace(R.id.main_frame, hospitalFragment).commit()
+            }
+        }
+
+        true
+    }
+
+    /*override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        Log.d(TAG, "MainActivity - onNavigationItemSelected() called")
+
+        when(item.itemId){
+            R.id.item_scan ->{
+                Log.d(TAG, "MainActivity - 스캔버튼!")
+            }
+            R.id.item_serch ->{
+                Log.d(TAG, "MainActivity - 검색버튼!")
+            }
+            R.id.item_hospital ->{
+                Log.d(TAG, "MainActivity - 병원버튼!")
+            }
+        }
+
+        return true
+    }*/
 }
---
+++ EatItDogAndroid/app/src/main/java/com/example/eatitdogandroid/SearchFragment.kt
@@ -0,0 +1,43 @@
+package com.example.eatitdogandroid
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class SearchFragment : Fragment() {
+    companion object{
+        const val TAG : String = "로그"
+
+        fun newInstance() : SearchFragment {
+            return SearchFragment()
+        }
+    }
+
+    //메모리에 올라갔을 때
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "SearchFragment - onCreate() called")
+    }
+
+    //프레그먼트를 안고 있는 액티비티에 붙었을 때
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        Log.d(TAG, "SearchFragment - onAttach() called")
+    }
+
+    //뷰가 생성 되었을 때
+    //프레그먼트와 레이아웃을 연결시켜주는 부분이다.
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        Log.d(TAG, "SearchFragment - onCreateView() called")
+
+        val view = inflater.inflate(R.layout.fragment_search, container, false)
+
+        return view
+    }
+
+
+}
